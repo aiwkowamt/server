4fa52ce27c6baf3fb97cac0dc95cfd38b2c89efa
aiwkowamt <radion.pohribniak@outlook.com>
2024-02-06 17:43:04 +0200

    commit

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::create([
+            'email' => 'admin@mail.com',
+            'password' => 'adminadmin',
+            'role_id' => 1,
+        ]);
+    }
+}
